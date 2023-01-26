79ecd1f50db2749632172178cf7ed9a4663c7034
Pobitrodeb <pobitrodn@gmail.com>
2023-01-26 22:44:56 +0600

    bootstrap install by VueJs command

---
+++ src/components/Calender.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+       <div class="container">
+        <h2>Calender App With VueJs </h2>
+       </div>
+    </section> 
+    <section>
+
+    </section>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
